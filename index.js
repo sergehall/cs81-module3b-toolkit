302d91d017b9f16bc76b8e21aca7d77620b8b596
serge <serge.hall.dev@gmail.com>
2025-07-03 16:22:31 -0700

    Add index.js to test math functions and document all function definitions
    - Created index.js to log outputs of all math functions using console.log
    - Added descriptive comments to each function in math.js for clarity
    - Documented module.exports section to explain usage

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+const { double, square, isEven, multiply, isOdd } = require("./math");
+// Run this file to see results of math functions in console
+
+// Assuming all functions are defined in the same file (math.js) for simplicity
+// If you're using modules, you'd import them here instead
+
+// Function calls
+
+console.log("double(4) =>", double(4));
+console.log("square(5) =>", square(5));
+console.log("isEven(6) =>", isEven(6));
+console.log("multiply(3, 7) =>", multiply(3, 7));
+console.log("isOdd(9) =>", isOdd(9));
